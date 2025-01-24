219322ca5dacc8d19131b1819e61d34640edd3c1
Herom03 <madalenoherom@yahoo.com>
2025-01-24 11:12:10 -0300

    Tela inicial EditText

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -17,20 +19,43 @@ class MainActivity : AppCompatActivity() {
         val edtAltura = findViewById<TextInputEditText>(R.id.edt_altura)
 
             // Recuperar o botão da tela
+
         val btn_calcular = findViewById<Button>(R.id.btn_calcular)
 
             //Colocar ação no botão setOnClickListener
+
         btn_calcular.setOnClickListener {
             //Recuperar o texto digitado no EditPeso e no EditAltura
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
 
-            val alturaQ2 = altura * altura
-            val resultado = peso / alturaQ2
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
 
-            println("Herom ação do botão" + resultado)
+            if(pesoStr == "" || alturaStr == "") {
+                // Mostrar mensagem pro usuário
 
+                Snackbar.make(
+                    edtPeso,
+                    "Preencha todos os campos",
+                    Snackbar.LENGTH_LONG
+                ).show()
+
+            }else{
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_RESULT_IMC, resultado)
+                startActivity(intent)
+
+                println("Herom ação do botão" + resultado)
+
+
+
+
+            }
         }
-
     }
 }
